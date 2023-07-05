abed4564e563b65f3b176583775a7e92b0e6efe3
jetnipat.t <jetnipat.t@nexthop.co.th>
2023-07-05 23:52:42 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth.guard';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { FreezePipe } from './pipes/freeze.pipe';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  // @UseInterceptors(LoggingInterceptor)
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  // @UseGuards(FreezePipe)
+  examplePost(@Body(new FreezePipe()) body: any) {
+    body.test = 32;
+  }
+
+  @Get('error')
+  throwError() {
+    throw new InternalServerErrorException();
+  }
+}
